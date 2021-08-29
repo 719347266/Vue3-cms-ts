8a008f579c88429d17d72e169a5daabf536d695b
lyh <719347266@qq.com>
2021-08-29 15:04:29 +0800

    fix: changeApiUrl

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -6,7 +6,7 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://coderwhy.org/prod'
+  BASE_URL = 'http://152.136.185.210:4000'
 } else {
   BASE_URL = 'http://coderwhy.org/test'
 }
