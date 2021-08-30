7aeea463046d2428fc5d26ecd9852e73d8ec4b76
719347266 <719347266@qq.com>
2021-08-30 10:39:57 +0800

    fix: 1

--- src/components/nav-menu/src/nav-menu.vue
+++ src/components/nav-menu/src/nav-menu.vue
@@ -2,7 +2,7 @@
   <div class="nav-menu">
     <div class="logo">
       <img class="img" src="~@/assets/img/logo.svg" alt="logo" />
-      <span v-if="!collapse" class="title">Vue3+TS-cms</span>
+      <span v-if="!collapse" class="title">Vue3+TS-cms-2</span>
     </div>
     <el-menu
       :default-active="defaultValue"
